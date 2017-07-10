bf36029157c663e6d8dbebbc0e9e0db84caa17b2
sdeepica <sdeeps1028@gmail.com>
2017-07-10 14:57:21 +0530

    Add files via upload

---
+++ fibonaci.java
@@ -0,0 +1,24 @@
+package codekata;
+
+import java.util.Scanner;
+
+public class fibonaci {
+	public static void main(String[] args){
+		int n1=0;
+		int n2=1;
+		int n3;
+		int i;
+		int count;
+		Scanner s=new Scanner(System.in);
+		count=s.nextInt();
+		System.out.print(n1+" "+n2);
+		for(i=2;i<count;i++){
+			n3=n1+n2;
+			System.out.print(" "+n3);
+			n1=n2;
+			n2=n3;
+		}
+		s.close();
+	}
+
+}
